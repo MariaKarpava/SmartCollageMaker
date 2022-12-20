0a8f530d284e2fefd46950a2fc41912cd832325e
MariaKarpava <mary.talabaeva@gmail.com>
2022-12-20 20:26:05 +0000

    refactoring: recize_and_crop_image()

--- resizing.py
+++ resizing.py
@@ -1,14 +1,13 @@
 from PIL import Image
 
 
-def my_center_crop(img, side_length):
-
-    image = Image.open(img)
-    print(image.size)  # (1280, 853) - car // (800, 1200) - christmas
+def recize_and_crop_image(image_to_resize_and_crop, side_length, resized_and_croped_image):
+    
+    image = Image.open(image_to_resize_and_crop)
+    # print(image.size)  # (1280, 853) - car // (800, 1200) - christmas
 
     width, height = image.size
-    print("w:", width)
-    print("h:", height)
+    
 
     if width > height:
         smaller_side = height
@@ -22,18 +21,15 @@ def my_center_crop(img, side_length):
 
     smaller_side = side_length
     larger_side = round(larger_side / factor)
-    print("larger_side:", larger_side)
-    print("smallew_side:", smaller_side)
+    
 
     if width > height:
         image_resized = image.resize((larger_side, smaller_side))
-        image_resized.save(r"image_resized.jpeg")
+        image_resized.save(resized_and_croped_image)
     else:
         image_resized = image.resize((smaller_side, larger_side))
-        image_resized.save(r"image_resized.jpeg")
+        image_resized.save(resized_and_croped_image)
 
-    print(image_resized.size)
-    image_resized.show()
     
     """
     - найди средний пиксель 
@@ -43,58 +39,39 @@ def my_center_crop(img, side_length):
     """
 
     if width > height:
-        new_x_left = (larger_side / 2) - (side_length / 2)
-        new_y_upper = 0
+        left = (larger_side / 2) - (side_length / 2)
+        upper = 0
 
-        new_x_right = (larger_side / 2) + (side_length / 2)
-        new_y_left_lower = side_length
+        right = (larger_side / 2) + (side_length / 2)
+        lower = side_length
 
-        print("new_x_left ", new_x_left)
-        print("new_y_upper", new_y_upper)
-        print("new_x_right", new_x_right)
-        print("new_y_left_lower", new_y_left_lower)
-
-        with Image.open("image_resized.jpeg") as im:
-            (left, upper, right, lower) = (new_x_left, new_y_upper, new_x_right, new_y_left_lower)
+        with Image.open(resized_and_croped_image) as im:
             im_crop = im.crop((left, upper, right, lower))
+            im_crop.save(resized_and_croped_image)
 
     elif height > width:
 
-        new_x_left = 0     
-        new_y_upper = (larger_side / 2) - (side_length / 2)
+        left = 0     
+        upper = (larger_side / 2) - (side_length / 2)
 
-        new_x_right = side_length    
-        new_y_left_lower = (larger_side / 2) + (side_length / 2)
+        right = side_length    
+        lower = (larger_side / 2) + (side_length / 2)
 
-        print("new_x_left ", new_x_left)
-        print("new_y_upper", new_y_upper)
-        print("new_x_right", new_x_right)
-        print("new_y_left_lower", new_y_left_lower)
-
-        with Image.open("image_resized.jpeg") as im:
-            (left, upper, right, lower) = (new_x_left, new_y_upper, new_x_right, new_y_left_lower)
+        with Image.open(resized_and_croped_image) as im:
             im_crop = im.crop((left, upper, right, lower))
+            im_crop.save(resized_and_croped_image)
 
     elif height == width:
 
-        new_x_left = 0     
-        new_y_upper = 0
+        left = 0     
+        upper = 0
 
-        new_x_right = side_length    
-        new_y_left_lower = side_length
+        right = side_length    
+        lower = side_length
 
-        print("new_x_left ", new_x_left)
-        print("new_y_upper", new_y_upper)
-        print("new_x_right", new_x_right)
-        print("new_y_left_lower", new_y_left_lower)
-
-        with Image.open("image_resized.jpeg") as im:
-            (left, upper, right, lower) = (new_x_left, new_y_upper, new_x_right, new_y_left_lower)
+        with Image.open(resized_and_croped_image) as im:
             im_crop = im.crop((left, upper, right, lower))
+            im_crop.save(resized_and_croped_image)
 
 
-    print("im_crop.size", im_crop.size)
-    im_crop.show()
-
-
-my_center_crop("christmas.jpeg", 600)
+# recize_and_crop_image("christmas.jpeg", 600, "resized_and_croped_image.jpeg")
